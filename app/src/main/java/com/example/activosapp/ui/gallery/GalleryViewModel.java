6f645acfeba82337a3ef0049986e074424e88af0
jcprietoe <60022176+jcprietoe@users.noreply.github.com>
2020-02-02 08:21:47 -0500

    creacion del formulario, modulo 1 operacion y mantenimiento

--- app/src/main/java/com/example/activosapp/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/example/activosapp/ui/gallery/GalleryViewModel.java
@@ -8,10 +8,7 @@ public class GalleryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
-    }
+
 
     public LiveData<String> getText() {
         return mText;
