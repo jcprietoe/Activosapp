7051638b90825eb46d880e7259d69fd1e5096f2e
jcprietoe <jcprietoe@hotmail.com>
2020-02-16 01:15:06 -0500

    arreglo vista de creacion de activo, creacion para la seleccion de imagenes,

--- app/src/main/java/com/example/activosapp/ui/crear_activo/CrearActivo.java
+++ app/src/main/java/com/example/activosapp/ui/crear_activo/CrearActivo.java
@@ -1,8 +1,15 @@
 package com.example.activosapp.ui.crear_activo;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +19,36 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.activosapp.R;
 import com.example.activosapp.VolleyRP;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class CrearActivo extends Fragment {
@@ -41,16 +60,27 @@ public class CrearActivo extends Fragment {
     private RequestQueue mRequest;
     // declaracion de variables
 
+    Button btnBuscarImagen;
+    ImageView ivMostrarImagen;
+
+
+    Bitmap bitmap;
+    int PICK_IMAGE_REQUEST = 1;
+    String UPLOAD_URL = "http://IP_DEL_WEBSERVICE/ARCHIVO_QUE_RECIBE_LA_PETICION.php";
+
+    String KEY_IMAGE = "foto";
+    String KEY_NOMBRE = "nombre";
+
     ArrayList<String>listPrueba;
     private String URL_LISTA_Documento = "http://www.gerenciandomantenimiento.com/activos/mantenimientoapp/obtenerTipoDocumento.php";
     ArrayAdapter<String> aaTipoDocumento;
     View vista;
     Spinner spinnerdocu;
     String[] tpodocumento;
-    EditText fechamatricula, fechafabricacion, edtvalorreal, edtcodigointerno, edtnoplaca, edtnodoctercero, edtemailtercero, edtteltercero;
+    EditText fechamatricula, fechafabricacion, edtvalorreal, edtcodigointerno, edtnoplaca, edtnodoctercero, edtemailtercero, edtteltercero,edtNombreImagen;
     EditText edtmodelo, edtreferencia, edtlinea, edtserial, edtserialmotor, edtserialpartes, edtnombretercero, edtdescripcion, edtubicacion;
-    RadioButton rbtnsi, rbtnno;
-    Button btn_guardar_registro;
+
+    Button btn_guardar_registro,btnAdjuntarImagen;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -62,7 +92,7 @@ public class CrearActivo extends Fragment {
         tpodocumento = getResources().getStringArray(R.array.tipo_documentos);
         fechamatricula = vista.findViewById(R.id.edtfmatricula);
         fechafabricacion = vista.findViewById(R.id.edtfabricacion);
-
+        btnBuscarImagen=vista.findViewById(R.id.btnBuscarImagen);
         edtnoplaca = vista.findViewById(R.id.edtnoplaca);
         edtnodoctercero = vista.findViewById(R.id.edtnodoctercero);
         edtemailtercero = vista.findViewById(R.id.edtemailtercero);
@@ -75,8 +105,8 @@ public class CrearActivo extends Fragment {
         edtserialpartes = vista.findViewById(R.id.edtserialpartes);
         edtnombretercero = vista.findViewById(R.id.edtnombretercero);
         edtdescripcion = vista.findViewById(R.id.edtdescripcion);
-
-
+        ivMostrarImagen=vista.findViewById(R.id.ivMostrarImagen);
+        edtNombreImagen=vista.findViewById(R.id.edtNombreImagen);
         btn_guardar_registro = vista.findViewById(R.id.btn_guardar_registro);
 
 
@@ -94,10 +124,19 @@ public class CrearActivo extends Fragment {
             }
         });
 
+        btnBuscarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+
         //boton guardar
         btn_guardar_registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                uploadImage();
 
                 Toast.makeText(getContext(), "entro aqui", Toast.LENGTH_SHORT).show();
 
@@ -212,4 +251,76 @@ public class CrearActivo extends Fragment {
             //populateSpinner();
         }
     }
+
+
+    public String getStringImagen(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    public void uploadImage() {
+        final ProgressDialog loading = ProgressDialog.show(getContext(), "Subiendo...", "Espere por favor");
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loading.dismiss();
+                        Toast.makeText(getContext(), response, Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loading.dismiss();
+                Toast.makeText(getContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                String imagen = getStringImagen(bitmap);
+                String nombre = edtNombreImagen.getText().toString().trim();
+
+                Map<String, String> params = new Hashtable<String, String>();
+                params.put(KEY_IMAGE, imagen);
+                params.put(KEY_NOMBRE, nombre);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleciona imagen"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Cómo obtener el mapa de bits de la Galería
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Configuración del mapa de bits en ImageView
+                ivMostrarImagen.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private ContentResolver getContentResolver() {
+
+        return getContentResolver();
+    }
+
 }
